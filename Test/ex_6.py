108404417645507eb5183d1c672fecf8b3ddce1c
tjhlp <374586186@qq.com>
2020-03-29 18:40:17 +0800

    add exercise_5

--- Test/ex_6.py
+++ Test/ex_6.py
@@ -13,3 +13,10 @@
 链接：https://leetcode-cn.com/problems/available-captures-for-rook
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 """
+
+
+class Solution:
+    def numRookCaptures(self, board) -> int:
+        return (lambda x: x('pR') + x('Rp'))(
+            [''.join(board[x] + [' '] + [i[y] for i in board]).replace('.', '') for x in range(8) for y in range(8) if
+             board[x][y] == 'R'][0].count)
