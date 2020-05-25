c40106a2f29ff82e97403aa96ef710bf467fc8ba
tjhlp <374586186@qq.com>
2020-05-25 14:35:13 +0800

    ex_21

---
+++ test/trade_ccxt/demo_1.py
@@ -0,0 +1,45 @@
+import requests
+import pandas as pd
+from datetime import datetime
+
+base_url = "https://api.binancezh.com/"
+
+
+def get_ticker_24h(symbol):
+    """
+        24hr 价格变动情况
+    """
+    if symbol is None:
+        url = base_url + 'api/v3/ticker/24hr'
+    else:
+        symbol.replace('_', '').upper()
+        url = base_url + 'api/v3/ticker/24hr?symbol={}'.format(symbol.replace('_', '').upper())
+
+    try:
+        rep_obj = requests.get(url)
+    except Exception as e:
+        print("错误：", e)
+        return
+
+    if rep_obj.status_code == 200:
+        rep_json = rep_obj.json()
+        print(rep_json)
+        raw_df = pd.DataFrame(9).set_index(0)
+
+        ticker_df = pd.DataFrame()
+        print(raw_df)
+    else:
+        print("状态码错误:", rep_obj.status_code)
+
+
+
+
+
+def main():
+    # symbol = 'btc_usdt'
+    symbol = None
+    get_ticker_24h(symbol)
+
+
+if __name__ == '__main__':
+    main()
