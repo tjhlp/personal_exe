0dd64886da64c59f234a205e2aa9e42effac57e5
zkzz2020_shikaining <shikaining@zkzz.onaliyun.com>
2020-11-27 11:20:41 +0800

    add quantization

---
+++ quantization/test1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# File              : test1.py
+# Author            : tjh
+# Create Date       : 2020/11/23
+# Last Modified Date: 2020/11/23
+# Last Modified By  : tjh
+# Reference         :
+# Description       :
+# ******************************************************
+
+
