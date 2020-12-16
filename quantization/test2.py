0f7a21c95f5644e685e7db8d8e0752b20295635c
zkzz2020_shikaining <shikaining@zkzz.onaliyun.com>
2020-12-16 16:27:46 +0800

    add get_hs

--- quantization/test2.py
+++ quantization/test2.py
@@ -7,5 +7,21 @@
 # Reference         :
 # Description       :
 # ******************************************************
+"""
 
+利用http://tushare.org/index.html 中介绍的api，爬取
+http://tushare.org/trading.html 页面中的所有交易数据存入本地数
+据库（或csv文件夹）。
+hint: 参考 http://tushare.org/storing.html
+Due date：Next Week
+Bottom Line：会使用api
+
+"""
+
+import tushare as ts
+
+hs_stock = ts.get_hs300s()
+
+print(hs_stock)
+print(type(hs_stock))
 
