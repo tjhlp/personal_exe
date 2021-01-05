0bae6a7bf71bdbc527dd164cf98b4c0210b40e0a
zkzz2020_shikaining <shikaining@zkzz.onaliyun.com>
2021-01-05 18:06:27 +0800

    fix param

--- quantization/test1.py
+++ quantization/test1.py
@@ -7,30 +7,21 @@
 # Reference         :
 # Description       :
 # ******************************************************
+import datetime
 
 from sklearn.datasets import load_iris
-iris =load_iris()
+# iris =load_iris()
+#
+# print(iris)
+# t = iris.keys()
+# print(t)
+# print(datetime.datetime.now().strftime('%Y%m%d%H'))
+with open('./test.txt', 'w')as w:
+    w.write(str(9999))
 
-print(iris)
-t = iris.keys()
-print(t)
-print(t)
-print(t)
-print(t)
-print(t)
-print(t)
-print(t)
-print(t)
-print(t)
-print(t)
-print(t)
-print(t)
-print(t)
-print(t)
-print(t)
-print(t)
-print(t)
-print(t)
-print(t)
+for i in range(2):
+    with open('./test.txt', 'a')as w:
+        w.write(str(i))
+
 
 
---
+++ test_fate/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# File              : __init__.py.py
+# Author            : tjh
+# Create Date       : 2021/01/05
+# Last Modified Date: 2021/01/05
+# Last Modified By  : tjh
+# Reference         :
+# Description       :
+# ******************************************************
+
+
