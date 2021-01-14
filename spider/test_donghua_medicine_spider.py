e5e66b477ef18bf2b8ace9d8fbceabb94245b3d5
tjhlp <374586186@qq.com>
2021-01-14 10:31:29 +0800

    fix donghua_spider

--- spider/test_donghua_medicine_spider.py
+++ spider/test_donghua_medicine_spider.py
@@ -58,63 +58,74 @@ s_time = return_date('time')
 
 
 def get_goods_url(browser, url):
-    browser.get(url)
+    # browser.get(url)
     time.sleep(1)
     div_list = browser.find_elements_by_xpath('.//div[@class="gl-i-wrap"]')
-    rsp = []
     url_list = []
     for div in div_list:
         s = div.find_element_by_xpath('.//div[@class="jPic"]/a').get_attribute("href")
         url_list.append(s)
-        # browser.get(s)
-        # sku_name = browser.find_element_by_xpath('.//div[@class="sku-name"]').text
-        # sku_price = browser.find_element_by_xpath('.//span[@class="p-price"]/span[2]').text
-        # sku_brand = browser.find_element_by_xpath('.//ul[@id="parameter-brand"]/li').get_attribute("title")
-        # sku_type = browser.find_element_by_xpath('.//div[@class="p-parameter"]/ul[2]/li[7]').get_attribute("title")
-        # sku_model = browser.find_element_by_xpath('.//div[@class="p-parameter"]/ul[2]/li[8]').get_attribute("title")
-        # sku_ex = browser.find_element_by_xpath('.//div[@class="p-parameter"]/ul[2]/li[9]').get_attribute("title")
-        # res = {
-        #     "sku_name": sku_name,
-        #     "sku_price": sku_price,
-        #     "sku_brand": sku_brand,
-        #     "sku_type": sku_type,
-        #     "sku_model": sku_model,
-        #     "sku_ex": sku_ex,
-        # }
-        # rsp.append(res)
-        # print(res)
-
+    print(url_list)
     return url_list
-
-def get_goods_detail(browser, goods):
-    res = []
-    for rank, goods_info in goods.items():
-        pass
 
 
 url = 'https://mall.jd.com/view_search-1461791-13517749-99-1-20-1.html'
-url_list = get_goods_url(browser, url)
+#
+
+browser.get(url)
+page_num = browser.find_element_by_xpath('.//div[@id="J_GoodsList"]/div/a[4]').text
+print(page_num)
 rsp = []
-for good_url in url_list:
-    browser.get(good_url)
-    sku_name = browser.find_element_by_xpath('.//div[@class="sku-name"]').text
-    sku_price = browser.find_element_by_xpath('.//span[@class="p-price"]/span[2]').text
-    sku_brand = browser.find_element_by_xpath('.//ul[@id="parameter-brand"]/li').get_attribute("title")
-    sku_type = browser.find_element_by_xpath('.//div[@class="p-parameter"]/ul[2]/li[7]').get_attribute("title")
-    sku_model = browser.find_element_by_xpath('.//div[@class="p-parameter"]/ul[2]/li[8]').get_attribute("title")
-    sku_ex = browser.find_element_by_xpath('.//div[@class="p-parameter"]/ul[2]/li[9]').get_attribute("title")
-    res = {
-        "sku_name": sku_name,
-        "sku_price": sku_price,
-        "sku_brand": sku_brand,
-        "sku_type": sku_type,
-        "sku_model": sku_model,
-        "sku_ex": sku_ex,
-    }
-    rsp.append(res)
+for i in range(page_num):
+    url_list = get_goods_url(browser, url)
+
+    print("第%s页 %s 条记录" % (i, len(url_list)))
+    for good_url in url_list:
+        time.sleep(0.5)
+        print("%s" % good_url)
+        browser.get(good_url)
+        sku_name = browser.find_element_by_xpath('.//div[@class="sku-name"]').text
+        sku_price = browser.find_element_by_xpath('.//span[@class="p-price"]/span[2]').text
+        sku_brand = browser.find_element_by_xpath('.//ul[@id="parameter-brand"]/li').get_attribute("title")
+        try:
+            sku_type = browser.find_element_by_xpath('.//div[@class="Ptable-item"]/dl/dl[1]/dd').text
+        except Exception as e:
+            print(e)
+            sku_type = ''
+        try:
+            sku_model = browser.find_element_by_xpath('.//div[@class="p-parameter"]/ul[2]/li[8]').get_attribute("title")
+        except Exception as e:
+            print(e)
+            sku_model = ''
+        try:
+            sku_ex = browser.find_element_by_xpath('.//div[@class="p-parameter"]/ul[2]/li[9]').get_attribute("title")
+        except Exception as e:
+            print(e)
+            sku_ex = ''
+        res = {
+            "sku_url": good_url,
+            "sku_name": sku_name,
+            "sku_price": sku_price,
+            "sku_brand": sku_brand,
+            "sku_model": sku_model,
+            "sku_ex": sku_ex,
+        }
+        rsp.append(res)
+        print(res)
+
+    # 点击下一页
+    try:
+        browser.execute_script("var q=document.documentElement.scrollTop=100000")
+        next_page = browser.find_element_by_xpath('.//div[@id="J_GoodsList"]/div/a[5]')
+        browser.execute_script("arguments[0].click();", next_page)
+    except:
+        break
+print(page_num)
 
 print(rsp)
-
+df = pd.DataFrame(rsp,
+                  columns=["sku_url", "sku_name", 'sku_price', 'sku_brand', 'sku_model', 'sku_ex'])
+df.to_csv('./donghua_jd.csv', encoding='gbk', index=False)
 
 print('goods calc:%ss ' % (calc_time_interval(s_time, return_date('time'))))
 
