e52f40c5a58004f7e08fedf13f142401ca866874
tjhlp <374586186@qq.com>
2021-01-14 11:25:26 +0800

    fix donghua_spider

--- spider/test_donghua_medicine_spider.py
+++ spider/test_donghua_medicine_spider.py
@@ -19,7 +19,7 @@ options = webdriver.ChromeOptions()
 options.add_argument(
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36')
 
-# options.add_argument('--headless')
+options.add_argument('--headless')
 browser = webdriver.Chrome('./chromedriver.exe', chrome_options=options)
 
 num = 1
@@ -65,54 +65,24 @@ def get_goods_url(browser, url):
     for div in div_list:
         s = div.find_element_by_xpath('.//div[@class="jPic"]/a').get_attribute("href")
         url_list.append(s)
-    print(url_list)
     return url_list
 
 
 url = 'https://mall.jd.com/view_search-1461791-13517749-99-1-20-1.html'
-#
 
 browser.get(url)
+browser.execute_script("var q=document.documentElement.scrollTop=100000")
 page_num = browser.find_element_by_xpath('.//div[@id="J_GoodsList"]/div/a[4]').text
-print(page_num)
+print("总共：%s页 " % page_num)
 rsp = []
-for i in range(page_num):
-    url_list = get_goods_url(browser, url)
-
-    print("第%s页 %s 条记录" % (i, len(url_list)))
-    for good_url in url_list:
-        time.sleep(0.5)
-        print("%s" % good_url)
-        browser.get(good_url)
-        sku_name = browser.find_element_by_xpath('.//div[@class="sku-name"]').text
-        sku_price = browser.find_element_by_xpath('.//span[@class="p-price"]/span[2]').text
-        sku_brand = browser.find_element_by_xpath('.//ul[@id="parameter-brand"]/li').get_attribute("title")
-        try:
-            sku_type = browser.find_element_by_xpath('.//div[@class="Ptable-item"]/dl/dl[1]/dd').text
-        except Exception as e:
-            print(e)
-            sku_type = ''
-        try:
-            sku_model = browser.find_element_by_xpath('.//div[@class="p-parameter"]/ul[2]/li[8]').get_attribute("title")
-        except Exception as e:
-            print(e)
-            sku_model = ''
-        try:
-            sku_ex = browser.find_element_by_xpath('.//div[@class="p-parameter"]/ul[2]/li[9]').get_attribute("title")
-        except Exception as e:
-            print(e)
-            sku_ex = ''
-        res = {
-            "sku_url": good_url,
-            "sku_name": sku_name,
-            "sku_price": sku_price,
-            "sku_brand": sku_brand,
-            "sku_model": sku_model,
-            "sku_ex": sku_ex,
-        }
-        rsp.append(res)
-        print(res)
-
+url_list = []
+for i in range(int(page_num)):
+    time.sleep(1)
+    div_list = browser.find_elements_by_xpath('.//div[@class="gl-i-wrap"]')
+    for div in div_list:
+        s = div.find_element_by_xpath('.//div[@class="jPic"]/a').get_attribute("href")
+        url_list.append(s)
+    print("第%s页 %s 条记录" % (i, len(div_list)))
     # 点击下一页
     try:
         browser.execute_script("var q=document.documentElement.scrollTop=100000")
@@ -120,9 +90,39 @@ for i in range(page_num):
         browser.execute_script("arguments[0].click();", next_page)
     except:
         break
-print(page_num)
-
-print(rsp)
+num = 1
+for good_url in url_list:
+    time.sleep(0.5)
+    print("第%s个" % num)
+    browser.get(good_url)
+    sku_name = browser.find_element_by_xpath('.//div[@class="sku-name"]').text
+    sku_price = browser.find_element_by_xpath('.//span[@class="p-price"]/span[2]').text
+    sku_brand = browser.find_element_by_xpath('.//ul[@id="parameter-brand"]/li').get_attribute("title")
+    try:
+        sku_type = browser.find_element_by_xpath('.//div[@class="Ptable-item"]/dl/dl[1]/dd').text
+    except Exception as e:
+        print(e)
+        sku_type = ''
+    try:
+        sku_model = browser.find_element_by_xpath('.//div[@class="p-parameter"]/ul[2]/li[8]').get_attribute("title")
+    except Exception as e:
+        print(e)
+        sku_model = ''
+    try:
+        sku_ex = browser.find_element_by_xpath('.//div[@class="p-parameter"]/ul[2]/li[9]').get_attribute("title")
+    except Exception as e:
+        print(e)
+        sku_ex = ''
+    res = {
+        "sku_url": good_url,
+        "sku_name": sku_name,
+        "sku_price": sku_price,
+        "sku_brand": sku_brand,
+        "sku_model": sku_model,
+        "sku_ex": sku_ex,
+    }
+    rsp.append(res)
+    num += 1
 df = pd.DataFrame(rsp,
                   columns=["sku_url", "sku_name", 'sku_price', 'sku_brand', 'sku_model', 'sku_ex'])
 df.to_csv('./donghua_jd.csv', encoding='gbk', index=False)
